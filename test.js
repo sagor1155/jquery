45e5cf90cc00c7b03cabbb350ce7f8c577cce331
sagor1155 <sagor.eee.cuet@gmail.com>
2020-03-23 14:59:54 +0600

    jquery library attached

---
+++ test.js
@@ -0,0 +1,23 @@
+$(document).ready( function(){
+    alert("document is ready");
+});
+
+
+/*
+jquery selectors:
+-----------------
+$("*")          -   selects all element 
+$(this)         -   selects the current HTML element  
+$("p.intro")    -   selects <p> element with class .intro
+$("p:first")    -   selects the first <p> element 
+$("ul li:first")            -   selects first <li> of the first <ul>
+$("ul li:first-child")      -   selects first <li> of every <ul>
+$("[href]")                 -   selects all elements with href attribute 
+$("a[target='_blank']")     -   selects all <a> elements with a target attribute value equal to "_blank"
+$("a[target!='_blank']")    -   selects all <a> elements with a target attribute value NOT equal to "_blank"
+$(":button")    -   selects all <button> elements and <input> elements of type="button"
+$("tr:even")    -   selects all even <tr> elements 
+$("tr:odd")     -   selects all odd <tr> elements 
+*/
+
+    
